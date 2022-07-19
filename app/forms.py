3fd4d882787d45fa13d0e4b2575309a1d5d5397b
otnielgusta <git.github.com>
2022-07-19 09:30:15 -0300

    formulário de login feito

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired, length
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    remenber_me = BooleanField("Remember Me")
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from crypt import methods
 from turtle import pos
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 from app import app
+from app.forms import LoginForm
 from teste import Controller
 
 @app.route('/')
@@ -19,3 +21,11 @@ def index():
     ]
 
     return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form)
