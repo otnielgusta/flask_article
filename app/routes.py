8941b5dc77a4f6879b0626e46d9b263790b935a9
otnielgusta <git.github.com>
2022-07-27 09:53:26 -0300

    Criação dos métodos relacionados a criaçao e decodificaçao da senha.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from crypt import methods
-from turtle import pos
-from flask import flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for, request
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+#parou onde implementa a próxima linha
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {"username": "Otniel"}
     posts = [
@@ -23,8 +26,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or user.check_password(form.password.data):
+            flash('Usuário ou senha inválido')
+            return redirect(url_for('login'))
     return render_template('login.html', title="Sign In", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
